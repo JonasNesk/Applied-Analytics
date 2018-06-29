1cd856a5b5b372c9fe80f4f275d9c68e6193eda8
Jonas Neskudla <Jonas.Neskudla@gmx.de>
2018-06-29 18:31:34 +0200

    fast

--- convert_csv.py
+++ convert_csv.py
@@ -1,19 +1,34 @@
 import csv
+import inspect, os
+from pathlib import Path
 
-with open('/Users/Jonas/Documents/Studium/Master/SS18/Applied Analytics/Challenge/Data/crypto/ada.csv','r') as csvinput:
-    with open('/Users/Jonas/Documents/Studium/Master/SS18/Applied Analytics/output.csv', 'w') as csvoutput:
-        writer = csv.writer(csvoutput, lineterminator='\n')
-        reader = csv.reader(csvinput)
+for file in os.listdir("/Users/Jonas/Documents/Studium/Master/SS18/Applied Analytics/Challenge/Data/crypto"):
+    if file.endswith(".csv"):
+        filename = os.path.splitext(file)[0] #ohne endung
+        filename2 = (os.path.normpath(file)) # mit endung
 
-        all = []
-        row = next(reader)
-        row.append('Name_Waehrung')
-        all.append(row)
+        #filepath_in = os.path.realpath(file) schneidet letzte zwei Ordner weg, deswegen drunter
+        filepath_in = os.path.dirname(os.path.abspath(file)) + "/Data/crypto/" + filename2
+        filepath_out = filepath_in + filename + ".csv"
+        print(filepath_out)
+        
 
-        for row in reader:
-            row[0] = 'motherfucker'
-            row.append(row[0])
-            all.append(row)
-            
+        #filepath_in = '/Users/Jonas/Documents/Studium/Master/SS18/Applied Analytics/Challenge/Data/crypto/ada.csv'
+        with open(filepath_in,'r') as csvinput:
+            with open(filepath_out, 'w') as csvoutput:
+                writer = csv.writer(csvoutput, lineterminator='\n')
+                reader = csv.reader(csvinput)
 
-        writer.writerows(all)
+                all = []
+                row = next(reader)
+
+                row.append('currencyName')
+                all.append(row)
+                row_length = len(row)
+
+                for row in reader:
+                    row[row_length-1] = 'motherfucker'
+                    all.append(row)
+                
+                writer.writerows(all)
+
